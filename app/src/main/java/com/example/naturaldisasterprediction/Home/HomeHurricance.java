2197fe325fee3480b7acbe74b2695c4fa5e8222f
Nguyen Phuc Bao Uyen <95065083+Hollyuyen@users.noreply.github.com>
2024-10-17 01:47:43 +0700

    Home tmp

--- app/src/main/java/com/example/naturaldisasterprediction/Home/HomeHurricance.java
+++ app/src/main/java/com/example/naturaldisasterprediction/Home/HomeHurricance.java
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.naturaldisasterprediction.R;
 
-public class HomeBasic extends AppCompatActivity {
+public class HomeHurricance extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home_basic);
+        setContentView(R.layout.activity_home_hurricance);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
